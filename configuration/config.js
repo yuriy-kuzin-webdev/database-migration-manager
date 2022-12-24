dc8af78482c69204c16eabcf359377b805300cac
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2022-12-24 17:04:11 +0200

    updated config file

--- configuration/config.js
+++ configuration/config.js
@@ -3,14 +3,16 @@ const config = {
     input: {
         driver: "mongodb",
         connectionString: "mongodb+srv://virok:virok@cluster0.2z17wno.mongodb.net/?retryWrites=true&w=majority",
-        collections: "warehouse"
+        database: "warehouse",
+        collections: "goods"
     },
 
     outputs: [
         {
             driver: "mongodb",
             connectionString: "mongodb+srv://virok:virok@cluster0.2z17wno.mongodb.net/?retryWrites=true&w=majority",
-            collections: "warehouseTest"
+            database: "warehouse",
+            collections: "goodsCopy"
         },
     ],
 
